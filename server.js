8b9d622c943a36f436ee4359839d86441b340542
jerrydapoo <hendrix.chino@gmail.com>
2024-10-31 00:18:42 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3000; // Use Render's PORT environment variable
 
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://garycantilang:<db_password>@cluster0.cygdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; // Replace with your connection string
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = "mongodb+srv://garycantilang:<db_password>@cluster0.cygdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true"; // Replace with your connection string
+const client = new MongoClient(uri);
 
 async function connectToDatabase() {
     try {
